3007a148f70d9e50f1c5b563cded3fe0ac591e12
Ayush Aryan <ayusharyan1309@gmail.com>
2024-04-30 10:04:16 +0530

    first commit

---
+++ src/test/java/com/ayush/jenkins/dockerjenkinsintegration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.jenkins.dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
